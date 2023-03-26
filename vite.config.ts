eb6d1c5dd3590ee3452b4956a1cc5aee4858434b
AifaDev <83997447+AifaDev@users.noreply.github.com>
2023-03-26 04:14:34 +0300

    pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Chess Square Color Quiz",
+        short_name: "Chess Quiz",
+        description:
+          "A quiz app that asks you to guess the color of a chess square",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icons/chessboard.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
